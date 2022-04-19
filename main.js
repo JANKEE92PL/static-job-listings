2c34c85035d8ce0063c6e29c4939048f3b1fb426
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-04-19 19:07:29 +0200

    finished json

--- main.js
+++ main.js
@@ -6,49 +6,48 @@ const render = () => {
     .then((response) => response.json())
     .then((dataset) =>
       dataset.forEach((data) => {
-        data;
+        createJob(data);
       })
     );
 };
 
 render();
 
-const createJob = () => {
+const createJob = (object) => {
   // erstelle ein neues div Element
   // und gib ihm etwas Inhalt
   let insert = `<div class="job">
       <div class="box-left">
         <div class="box-logo">
-          <div class="logo"><img src="images/${logo}" alt="logo"></div>
+          <div class="logo"><img src="images/${object.logo}" alt="logo"></div>
         </div>
         <div class="box-description">
           <div class="job-header">
-            <h2 class="company">Photosnap</h2>
-            <p class="chip">New!</p>
-            <p class="chip">Featured</p>
+            <h2 class="company">${object.company}</h2>
+            <p class="chip">${object.keywords[0]}</p>
+            <p class="chip">${object.keywords[1]}</p>
+            <p class="chip">${object.keywords[2]}</p>
           </div>
 
           <div class="job-title">
-            <h2 class="title">Senior Frontend Developer</h2>
+            <h2 class="title">${object.title}</h2>
           </div>
           <div class="job-info">
-            <p class="date">1d ago</p>
-            <p class="time">Full Time</p>
-            <p class="place">USA only</p>
+            <p class="date">${object.info.date}</p>
+            <p class="time">${object.info.time}</p>
+            <p class="place">${object.info.place}</p>
           </div>
         </div>
       </div>
       <div class="box-right">
         <div class="job-description">
-          <p class="role">Frontend</p>
-          <p class="level">Senior</p>
-          <p class="language">HTML</p>
-          <p class="language">CSS</p>
-          <p class="language">JavaScript</p>
+          <p class="role">${object.role}</p>
+          <p class="level">${object.level}</p>
+          <p class="language">${object.languages[0]}</p>
+          <p class="language">${object.languages[1]}</p>
+          <p class="language">${object.languages[2]}</p>
         </div>
       </div>
     </div>`;
   wrapper.insertAdjacentHTML("beforeend", insert);
 };
-
-createJob();
